f7cd2c4ec12ebad2c7e6fbd1f33bcfbc9f496521
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-01 16:23:05 +0200

    add constants with min_length, compare length

--- src/main/java/com/agile/design/Password.java
+++ src/main/java/com/agile/design/Password.java
@@ -1,5 +1,18 @@
 package com.agile.design;
 
 public class Password {
+
+    private static final int MIN_LENGTH = 6;
+    private final String value;
+    protected Password(String value){
+        this.value = value;
+    }
+    public static Password create(String value) {
+        return new Password(value);
+    }
+
+    public boolean isStrong() {
+        return value.length() >= MIN_LENGTH;
+    }
     
 }
