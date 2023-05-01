03ba269e164c8cc13ab4752eb80472673ebcea8e
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-01 16:20:20 +0200

    create test: password is not strong when the size is less that six

--- src/test/java/com/agile/design/PasswordTest.java
+++ src/test/java/com/agile/design/PasswordTest.java
@@ -1,5 +1,14 @@
 package com.agile.design;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
 public class PasswordTest {
     
+    @Test
+    public void the_password_is_invalid_when_the_size_is_less_than_six(){
+        Password password = Password.create("aA3_");
+        assertFalse(password.isStrong());
+    }
 }
